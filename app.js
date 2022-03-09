96fb2fd5ba3d6c9ceddcea6f7a9b18750c102f25
Kamron <88978391+wieeil@users.noreply.github.com>
2022-03-09 11:25:17 +0500

    add token

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import TelegramBot from 'node-telegram-bot-api'
 import config from './config/config.js'
 import Registrate from './controllers/registrate.js'
 
-const bot = new TelegramBot(config.token,{ polling : true })
+const bot = new TelegramBot(`5186015669:AAF8xdrl9rVjn5xqvVjHlS5ggpo7QP4Juwc`,{ polling : true })
 
 Registrate.registrate(bot)
